e0510bcef7e9f3243b7357f049f4b5d2bbb25ecf
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-09-03 11:16:35 -0400

    show and hide cart with event listeners

--- app.js
+++ app.js
@@ -86,6 +86,7 @@ class UI {
         // display cart item
         this.addCartItem(cartItem);
         // show the cart with overlay
+        this.showCart(cart);
       });
     });
   }
@@ -113,11 +114,33 @@ class UI {
 </div>
 <div>
   <i class="fas fa-chevron-up" data-id=${item.id}></i>
-  <p class="item-amount">1</p>
+  <p class="item-amount">${item.amount}</p>
   <i class="fas fa-chevron-down" data-id=${item.id}></i>
 </div>
 `;
+    cartContent.appendChild(div);
   }
+  showCart() {
+    cartOverlay.classList.add("transparentBcg");
+    cartDOM.classList.add("showCart");
+  }
+  hideCart(){
+    cartOverlay.classList.remove("transparentBcg");
+    cartDOM.classList.remove("showCart");
+  }
+  setupApp() {
+ cart = Storage.getCart()
+ this.setCartValues(cart)
+ this.populateCart(cart)
+ cartBtn.addEventListener('click', this.showCart)
+ closeCartBtn.addEventListener('click', this.hideCart)
+  }
+  populateCart(cart){
+    cart.forEach(item => {
+      this.addCartItem(item)
+    })
+  }
+
 }
 // local storage
 class Storage {
@@ -133,12 +156,16 @@ class Storage {
   static saveCart(cart) {
     localStorage.setItem("cart", JSON.stringify(cart));
   }
+  static getCart(){
+    return localStorage.getItem('cart') ? JSON.parse(localStorage.getitem('cart')) : []
+  }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   const ui = new UI();
   const products = new Products();
-
+  // setup App
+  ui.setupApp();
   // get all products
   products
     .getProducts()
