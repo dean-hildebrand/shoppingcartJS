a1515f843663db46cd4e1b06b50a70c713599163
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-09-03 12:13:45 -0400

    Update cart by removing single item using event bubbling

--- app.js
+++ app.js
@@ -146,6 +146,15 @@ class UI {
       this.clearCart();
     });
     // cart functionality
+    cartContent.addEventListener('click', e => {
+      if (e.target.classList.contains('remove-item')){
+        let removeItem = e.target
+        let id = removeItem.dataset.id
+        cartContent.removeChild(removeItem.parentElement.parentElement)
+        this.removeItem(id)
+
+      }
+    })
   }
   clearCart() {
     let cartItems = cart.map(item => item.id);
