27abe425e13679f6cf2098c43b06d56e8cbf87c9
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-09-04 11:12:03 -0400

    Add contentful API token

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+const client = contentful.createClient({
+  // This is the space ID. A space is like a project folder in Contentful terms
+  space: "vyqchc3n5vx5",
+  // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+  accessToken: "iZNPB42gpB9ZgRtooynyNNgOpYQoQLHTnIxrrhnpXpo"
+});
+
+console.log(client);
+
 // variables
 
 const cartBtn = document.querySelector(".cart-btn");
